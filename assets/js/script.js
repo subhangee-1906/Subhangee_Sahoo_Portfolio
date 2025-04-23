a14ce16c3612e00d733171f0a89818c2c0cb2f78
Subhangee Sahoo <shubh190601@gmail.com>
2025-04-23 21:39:29 +0530

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Nikhil Gupta";
+            document.title = "Portfolio | Subhangee Sahoo";
             $("#favicon").attr("href", "assets/images/.png");
         }
         else {
